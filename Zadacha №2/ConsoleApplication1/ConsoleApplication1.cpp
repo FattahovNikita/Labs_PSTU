44c88a8f52f030eb4b05e09dfe002ffe3bfdff3a
FattahovNikita <92542030+FattahovNikita@users.noreply.github.com>
2021-10-26 18:45:55 +0500

    Add files via upload

---
+++ Zadacha №2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x, y, k;
+	srand(time(NULL));
+	x = rand() % 100 + 1;
+	k = 7;
+	cout << "Загадано число от 1 до 100, у вас есть "<< k << " попыток чтобы отгадать его" << endl;
+	cout << "Введите число которое больше или равно загаданного" << endl;
+	for (int i = 1; i <= k; ++i) {
+		cin >> y;
+		if (x == y) {
+			cout << "Это загадонное число,вы победили" << endl;
+		}
+		if (y > x) {
+			cout << "Ваше число больше загаданного" << endl;
+			cout << "Осталось попыток:" << k-i << endl;
+		}
+		if (x > y) {
+			cout << "Ваше число меньше загаданного" << endl;
+			cout << "Осталось попыток:" << k-i << endl;
+		}
+	}
+	cout << "Попытки кончились, вы проиграли" << endl;
+	return 0;
+}
