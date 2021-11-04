9c43ff0592779c6b387aec07dcefb0fb85bd4400
FattahovNikita <92542030+FattahovNikita@users.noreply.github.com>
2021-11-04 15:27:19 +0500

    Add files via upload

---
+++ Zadacha 2.1/Zadacha 2.1/Zadacha 2.1.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int i = 1;                  // создаем переменную i = 1
+	cout << ++i * i++ << endl;  // в начале i увеличивается на 1,потом i * i == 2 * 2, и в конце i увеличивается на 1
+	return 0;
+}
---
+++ Zadacha 2.3/Zadacha 2.3/Zadacha 2.3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	const int z = 10;
+	int x;
+	x = 20;
+	cout << z << endl;   // выводим целочисленную константу
+	cout << x << endl;   // выводим целочисленную переменную
+	cout << --x << endl; // выводим переменную через декремент
+	return 0;
+}
